a40e993405686d5e40d77a8ae1188084b2a230a0
Moosa Raheel <moosarahil13@gmail.com>
2023-08-15 16:31:42 +0500

    404 not found add

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 import express from "express";
 import web from "./routes/web.js";
 import { join } from "path";
+import dotenv from "dotenv";
+
+//Configure environment variables
+dotenv.config();
 const app = express();
 
 // load router
 app.use("/", web);
 
+//set view engine
+app.set("view engine", "ejs");
+
 // load static files
 app.use("/", express.static(join(process.cwd(), "public")));
 
 app.get("*", (req, res) => {
-  res.render("notFound");
+  res.statusCode = 404;
+  res.statusMessage = "bad request";
+  res.render("notFound", { path: req.path.replace("/", "") });
 });
-//set view engine
-app.set("view engine", "ejs");
 
 // listen Server
 const port = process.env.PORT || 3000;
